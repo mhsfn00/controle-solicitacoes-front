6d41272abdae53d5436b806eaeb4816600ec0ea3
horus <horus.cristian@gmail.com>
2025-07-24 01:40:03 -0300

    feat: create new route and nav structure

---
+++ src/routes.tsx
@@ -0,0 +1,80 @@
+import type { ReactElement } from "react";
+import {
+  AcademicRegisterPage,
+  CourseRegistryPage,
+  DefenseRequestPage,
+  ExternalMemberRegistryPage,
+  HomePage,
+  ProfessorRegistryPage,
+} from "./pages";
+import {
+  CalendarIcon,
+  GraduationCapIcon,
+  UserRoundPenIcon,
+  UserRoundIcon,
+  BookOpenCheckIcon,
+  UserRoundPlusIcon,
+  TableCellsMergeIcon,
+  ScrollTextIcon,
+} from "lucide-react";
+
+interface RouteConfig {
+  path: string;
+  label?: string;
+  element?: ReactElement;
+  icon?: ReactElement;
+}
+
+interface RoutesProps {
+  [key: string]: RouteConfig;
+}
+
+export const routes: RoutesProps = {
+  home: {
+    path: "/",
+    label: "Início",
+    element: <HomePage />,
+    icon: <CalendarIcon />,
+  },
+  defenseRequest: {
+    path: "/defenseRequest",
+    label: "Solicitar Defesa",
+    element: <DefenseRequestPage />,
+    icon: <GraduationCapIcon />,
+  },
+  professor: {
+    path: "/professor",
+    label: "Cadastrar Professor",
+    element: <ProfessorRegistryPage />,
+    icon: <UserRoundPenIcon />,
+  },
+  aluno: {
+    path: "/aluno",
+    label: "Cadastrar Aluno",
+    element: <AcademicRegisterPage />,
+    icon: <UserRoundIcon />,
+  },
+  course: {
+    path: "/course",
+    label: "Cadastrar Curso",
+    element: <CourseRegistryPage />,
+    icon: <BookOpenCheckIcon />,
+  },
+  externalMember: {
+    path: "/external-member",
+    label: "Registro de Membro Externo",
+    element: <ExternalMemberRegistryPage />,
+    icon: <UserRoundPlusIcon />,
+  },
+  requestExam: {
+    path: "/",
+    label: "Solicitar Exame",
+    icon: <ScrollTextIcon />,
+  },
+  viewStatus: {
+    path: "/",
+    label: "Visualizar Status",
+    icon: <TableCellsMergeIcon />,
+  },
+  editPerfil: { path: "/", label: "Editar Perfil", icon: <UserRoundPenIcon /> },
+};
